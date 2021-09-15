50a8782fe8b3d0753a24fc7a3af16c00d3ec6661
dat nguyen <thedatnguyen20@gmail.com>
2021-09-15 07:13:50 +0700

    first

---
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.entity.CustomerEntity;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+	
+}
